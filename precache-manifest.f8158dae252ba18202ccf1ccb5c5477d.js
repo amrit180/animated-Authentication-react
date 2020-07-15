fd924cf41eb858bbfb7813e16e353ba11ca5a940
amrit180 <62633596+amrit180@users.noreply.github.com>
2020-07-15 18:35:48 +0530

    Updates

---
+++ precache-manifest.f8158dae252ba18202ccf1ccb5c5477d.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5656b1bfe2aa00988d862ba1490aa0e4",
+    "url": "/animated-Authentication-react/index.html"
+  },
+  {
+    "revision": "d922d4057ff3878ff4d0",
+    "url": "/animated-Authentication-react/static/css/main.8ea011e8.chunk.css"
+  },
+  {
+    "revision": "ed6628a2dfef8c084da8",
+    "url": "/animated-Authentication-react/static/js/2.ddc8c328.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/animated-Authentication-react/static/js/2.ddc8c328.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d922d4057ff3878ff4d0",
+    "url": "/animated-Authentication-react/static/js/main.8f66f151.chunk.js"
+  },
+  {
+    "revision": "a88f759ae59fb8ce7d73",
+    "url": "/animated-Authentication-react/static/js/runtime-main.acc4a477.js"
+  },
+  {
+    "revision": "342519a6c72330d4a6d50cca6bf5976e",
+    "url": "/animated-Authentication-react/static/media/signin.342519a6.svg"
+  },
+  {
+    "revision": "2bbcda1a05867aa8f867e930954954a1",
+    "url": "/animated-Authentication-react/static/media/signup.2bbcda1a.svg"
+  }
+]);
